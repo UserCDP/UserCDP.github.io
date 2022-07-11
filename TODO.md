16fb940362f853a9293f99ce39ae4e08b14fb708
UserCDP <cojocaru.daniela@icloud.com>
2022-07-11 14:11:10 +0300

    Made sections as cards with rounded corners, added gradient background, have o fix margin-top first container

--- TODO.md
+++ TODO.md
@@ -1,4 +1,1 @@
-## TO DO
-1. top bar (make it to resemble your Fedora interface -- Apple's website was similar to their OS)
-2. Make an animation with fading text on scroll
-3. Color scheme: strong colors (see mockup)
+
---
+++ script.js
@@ -0,0 +1,7 @@
+document.onload = function() {
+
+    var top_bar = document.getElementsByClassName('top-bar');
+    //var about = ;
+
+    document.getElementById('about').style.marginTop = "60px"; //top_bar.clientHeight;
+};
