e18ef72fa486dd8e99542b026278bbd9a8b3e586
UserCDP <cojocaru.daniela@icloud.com>
2022-07-12 12:00:15 +0300

    Added an indicator for more on each section, made the sections shorter on big screens, added animations on indicators 9the animation is just forwards

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.onload = function() {
+window.onload = () => {
 
-    var top_bar = document.getElementsByClassName('top-bar');
-    //var about = ;
+    var top_bar = document.querySelector(".top-bar");
+    var about = document.querySelector("#about");
 
-    document.getElementById('about').style.marginTop = "60px"; //top_bar.clientHeight;
+    about.style.marginTop =(top_bar.clientHeight + 10) + "px";
+
+    var projects_btn = document.querySelectorAll(".more");
+    projects_btn.forEach(element => {
+        element.onclick = () => {
+            console.log(element.parentNode);
+            element.classList.add('rotation');
+        }
+    });
 };
